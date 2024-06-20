4eefde94c08a6490a1c6687b5da0513b5b53a262
Kamalakar Satapathi <103712713+Sgvkamalakar@users.noreply.github.com>
2024-06-21 01:20:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -144,6 +144,8 @@ def main():
     submit_button = st.button("Submit Job")
     st.error("The resource group associated with this project has been deactivated, resulting in the current non-functionality of the app 🤧... I apologize for any inconvenience caused 😔...")
     st.info("If you require further assistance or have any questions, feel free to reach out to me at sgvkamalakar@gmail.com")
+    st.subheader("Sample Video")
+    st.video("demo/demo.webm")
     if submit_button:
         if text_input.strip()!='':
             with st.spinner("Processing..."):
